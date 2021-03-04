65b0cac3c8ea5c31c33a17864a3896ef1105da84
wujiatao <wujiatao@do1.com.cn>
2021-03-04 22:57:53 +0800

    实现点击登录跳转到admin页面

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -48,7 +48,10 @@
                             <span class="lbl">记住我</span>
                           </label>
 
-                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                          <button type="button"
+                                  class="width-35 pull-right btn btn-sm btn-primary"
+                                  v-on:click="login()"
+                          >
                             <i class="ace-icon fa fa-key"></i>
                             <span class="bigger-110">登陆</span>
                           </button>
@@ -72,6 +75,11 @@
   $('body').attr('class', 'login-layout light-login');
 export default {
   name: 'login',
+    methods: {
+      login () {
+          this.$router.push("/admin")
+      }
+    }
 }
 </script>
 
