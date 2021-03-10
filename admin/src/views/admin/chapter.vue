d6149b297474d26218133918168f86e0639970ff
wujiatao <wujiatao@do1.com.cn>
2021-03-11 00:42:58 +0800

    5.6 增加新增大章功能的前端页面

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <p>
+      <button v-on:click="add()" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-edit"></i>
+        新增
+      </button>
+      &nbsp;
       <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
         <i class="ace-icon fa fa-refresh"></i>
         刷新
       </button>
     </p>
+    <!-- 单独的样式每页8条 -->
     <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
 
     <table id="simple-table" class="table  table-bordered table-hover">
@@ -80,9 +86,41 @@
       </tr>
       </tbody>
     </table>
+    <!-- /.modal 模态框-->
+    <div class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">表单</h4>
+          </div>
+          <div class="modal-body">
+            <form class="form-horizontal">
+              <div class="form-group">
+                <label class="col-sm-2 control-label">名称</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" placeholder="名称">
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label class="col-sm-2 control-label">课程id</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" placeholder="课程id">
+                </div>
+              </div>
+
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+            <button type="button" class="btn btn-primary">保存</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+
   </div>
-
-
 </template>
 
 <script>
@@ -105,6 +143,15 @@
 
         },
         methods: {
+            /**
+             * 点击【新增】
+             */
+            add() {
+                let _this = this;
+                _this.chapter = {};
+                $('.modal').modal('show');
+            },
+
             list(page) {
                 let _this = this;
                 _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list',{
