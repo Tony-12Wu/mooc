b6ce9223b344dfcce15e2a4e62c67ed6532881d0
wujiatao <wujiatao@do1.com.cn>
2021-03-10 17:22:07 +0800

    使用gateway路由转发，Vue页面只访问gateway的端口

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -95,7 +95,7 @@
         methods: {
             list() {
                 let _this = this;
-                _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then((response)=>{
+                _this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/list').then((response)=>{
                     console.log("查询大章列表结果:", response.data);
                     _this.chapters = response.data;
                 })
