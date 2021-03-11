541fe923aeedb99c99449162253b20730585fbd7
wujiatao <wujiatao@do1.com.cn>
2021-03-11 17:11:40 +0800

    5.8 a. 优化Toast组件 b. 制作Confirm组件

--- admin/public/static/js/toast.js
+++ admin/public/static/js/toast.js
@@ -1,32 +1,29 @@
-const Toast = Swal.mixin({
-  toast: true,
-  position: 'top-end',
-  showConfirmButton: false,
-  timer: 3000,
-  timerProgressBar: true,
-  didOpen: (toast) => {
-    toast.addEventListener('mouseenter', Swal.stopTimer)
-    toast.addEventListener('mouseleave', Swal.resumeTimer)
-  }
-})
-
-toast = {
+Toast = {
   success: function (message) {
-    Toast.fire({
+    Swal.fire({
+      position: 'top-end',
       icon: 'success',
-      title: message
+      title: message,
+      showConfirmButton: false,
+      timer: 1500
     })
   },
   warning: function (message) {
-    Toast.fire({
+    Swal.fire({
+      position: 'top-end',
       icon: 'warning',
-      title: message
+      title: message,
+      showConfirmButton: false,
+      timer: 1500
     })
   },
   error: function (message) {
-    Toast.fire({
-      icon: 'success',
-      title: message
+    Swal.fire({
+      position: 'top-end',
+      icon: 'error',
+      title: message,
+      showConfirmButton: false,
+      timer: 1500
     })
   }
   
