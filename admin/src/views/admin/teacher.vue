cc36970c2361d6b6487df9663ad6b6e6cc8310ec
wujiatao <wujiatao@do1.com.cn>
2021-03-23 18:13:22 +0800

    8.7 文件上传成功后返回FileDto给前端

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -239,7 +239,7 @@
 
             afterUpload(resp) {
                 let _this = this;
-                let image = resp.content;
+                let image = resp.content.path;
                 _this.teacher.image = image;
             },
 
--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -71,7 +71,8 @@ public class UploadController {
 
 
         ResponseDto responseDto = new ResponseDto();
-        responseDto.setContent(FILE_DOMAIN + path);
+        fileDto.setPath(FILE_DOMAIN + path);
+        responseDto.setContent(fileDto);
         return responseDto;
     }
 
