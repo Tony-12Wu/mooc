2a6b0c1694c7d2cb909d98a83163b8ac5807d371
wujiatao <wujiatao@do1.com.cn>
2021-03-24 21:12:32 +0800

    9.1 增加文件分片组件big-file.vue, 文件表增加分片相关字段

--- admin/src/views/admin/section.vue
+++ admin/src/views/admin/section.vue
@@ -79,11 +79,11 @@
                             <div class="form-group">
                               <label class="col-sm-2 control-label">视频</label>
                               <div class="col-sm-10">
-                                <file v-bind:input-id="'video-upload'"
-                                      v-bind:text="'上传视频'"
+                                <big-file v-bind:input-id="'video-upload'"
+                                      v-bind:text="'上传大视频'"
                                       v-bind:suffixs="['mp4']"
                                       v-bind:use="FILE_USE.COURSE.key"
-                                      v-bind:after-upload="afterUpload"></file>
+                                      v-bind:after-upload="afterUpload"></big-file>
                                 <div v-show="section.video" class="row">
                                   <div class="col-md-9">
                                     <video v-bind:src="section.video" id ="video" controls="controls"></video>
@@ -130,10 +130,10 @@
 <script>
     import Pagination from "../../components/pagination";
     import File from "../../components/file";
-
+    import BigFile from "../../components/big-file";
 
     export default {
-        components: {Pagination, File},
+        components: {Pagination, File, BigFile},
         name: "business-section",
         data: function () {
             return {
