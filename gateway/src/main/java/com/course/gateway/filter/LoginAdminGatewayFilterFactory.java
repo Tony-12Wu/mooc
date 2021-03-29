993a91a222a45c4304c7a4f0b662ae9d94e17d87
wujiatao <wujiatao@do1.com.cn>
2021-03-29 23:58:41 +0800

    11.7 gateway实现后台登录拦截

---
+++ gateway/src/main/java/com/course/gateway/filter/LoginAdminGatewayFilterFactory.java
@@ -0,0 +1,22 @@
+package com.course.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * @author JT
+ */
+@Component
+public class LoginAdminGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+
+    @Resource
+    LoginAdminGatewayFilter loginAdminGatewayFilter;
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return loginAdminGatewayFilter;
+    }
+}
