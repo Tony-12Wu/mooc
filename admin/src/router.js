fa9145bb13777f17742cb45166b92813bcf4bfef
Tony-12Wu <1501090251@qq.com>
2021-04-02 17:47:18 +0800

    12.3 资源树的补齐和角色模块的对接

--- admin/src/router.js
+++ admin/src/router.js
@@ -12,6 +12,7 @@ import File from './views/admin/file'
 import Content from './views/admin/content'
 import User from './views/admin/user'
 import Resource from './views/admin/resource'
+import Role from './views/admin/role'
 
 Vue.use(Router);
 
@@ -74,6 +75,10 @@ export default new Router({
             path: 'system/resource',
             name: 'system/resource',
             component: Resource
+        },{
+            path: 'system/role',
+            name: 'system/role',
+            component: Role
         }]
     }]
     }
