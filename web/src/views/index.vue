4a3fa082bf9c562e167a666378d5f9e04c533529
Tony-12Wu <1501090251@qq.com>
2021-04-12 10:56:21 +0800

    13.4 首页课程数据显示

--- web/src/views/index.vue
+++ web/src/views/index.vue
@@ -15,12 +15,12 @@
       <div class="container">
 
         <div class="row">
-          <div class="col-md-4">
+          <div v-for="o in news" class="col-md-4">
             <div class="card mb-4 shadow-sm">
-              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
+              <img class="img-fluid" v-bind:src="o.image">
               <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <H4>{{o.name}}</H4>
+                <p class="card-text">{{o.summary}}</p>
                 <div class="d-flex justify-content-between align-items-center">
                   <div class="btn-group">
                     <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
@@ -170,8 +170,46 @@
 
 <script>
     export default {
-        name: "index"
+        name: "index",
+        data: function () {
+            return {
+                news: [],
+            }
+        },
+        mounted() {
+            let _this = this;
+            _this.listNew();
+        },
+        methods: {
+            /**
+             * 查询新上好课
+             */
+            listNew() {
+                let _this = this;
+
+                // 新上好课不经常变，又经常被访问，适合用缓存
+                // 判断是否有缓存
+/*                let news = SessionStorage.get("news");
+                if (!Tool.isEmpty(news)) {
+                    _this.news = news;
+                    return;
+                }*/
+
+                _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/web/course/list-new').then((response)=>{
+                    console.log("查询新上好课结果：", response);
+                    let resp = response.data;
+                    if (resp.success) {
+                        _this.news = resp.content;
+                        // 保存到缓存
+                        //SessionStorage.set("news", _this.news);
+                    }
+                }).catch((response)=>{
+                    console.log("error：", response);
+                })
+            },
+        }
     }
+
 </script>
 
 <style scoped>
